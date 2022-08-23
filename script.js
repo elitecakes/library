c2fba128ab998bfb2756f3b7b7213d4e57c94439
elitecakes <joeotuairisg@gmail.com>
2022-08-23 12:10:09 +0100

    forgot about github, remove, read buttons working, ids attached to each book in library, null books cant be added, website looks a bit better

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const cardContainer = document.querySelector('.cardContainer');
 const newButton = document.getElementById('newBtn');
+const delButton = document.getElementById('deleteBtn');
+const editButton = document.getElementById('editBtn');
 const form = document.querySelector('.newForm');
 const bod = document.querySelector('.bodyContent');
 const addBookBtn = document.querySelector('.addBook');
+const bookCard = document.querySelector('.bookCard');
 
 let myBooks = [];
 
@@ -16,15 +19,111 @@ function Book(title, author, year, readStatus, pages, seriesStatus) {
 }
 
 function addBookToLibrary(bookobj) {
+
+    if (bookobj === 1) {
+        return 1;
+    }
+
     myBooks.push(bookobj);
     let div = document.createElement('div');
-    div.classList.add('bookCard');
-    cardContainer.appendChild(div);
-    div.innerText = `${bookobj.title}`;
+    div.classList.add('bookCard'); 
     
+    cardContainer.appendChild(div);
+
+    div.setAttribute("id", cardContainer.childElementCount - 1);
+    let currDiv = document.querySelector('.cardContainer').lastChild;
+
+
+    //Making book title appear in div for easier css alignment
+    let textDiv = document.createElement("div");
+    textDiv.classList.add('.textCont')
+    textDiv.innerText = `${bookobj.title}`;
+    currDiv.appendChild(textDiv);
+    
+
+    // Create a button that lives inside this creaated div
+    let readBtn = document.createElement("button");
+    readBtn.classList.add('cardBtn');
+    //if checkbox ticked, toggle read-unread
+    readBtn.classList.add('readBtn');
+    if (bookobj.readStatus === true) {
+        readBtn.classList.toggle('read-unread');
+    }
+    
+    console.log(bookobj);
+    readBtn.innerText = "Read";
+    currDiv.appendChild(readBtn);
+
+
+    let removeBtn = document.createElement("button");
+    removeBtn.classList.add('cardBtn', 'removeBtn');
+    removeBtn.innerText = "Remove";
+    currDiv.appendChild(removeBtn);
+
+    let infoBtn = document.createElement("button");
+    infoBtn.classList.add('cardBtn')
+    infoBtn.innerText = "Information";
+    currDiv.appendChild(infoBtn);
+
+    //add event listeners so these buttons do thier inteded functions
+    //read button
+    readBtn.addEventListener('click', function () {
+        readStatus(currDiv);
+    });
+
+    //remove button
+    removeBtn.addEventListener('click', function () {
+        removeBookFromLibrary(currDiv);
+        refreshID();
+    });
+
+    //make the card clickable for more info
+    infoBtn.addEventListener('click', function () {
+        showBookInfo(currDiv);
+    });
 }
 
+function removeBookFromLibrary (bookForDeletion) {
+    //Are you sure you want to delete this book message first
+    let bookID = bookForDeletion.getAttribute('id');
+    //remove from myBooks Array
+    myBooks.splice(bookID, 1);
+    //remove from cardContainter 
+    bookForDeletion.remove();
+}
 
+/*function addSelectorsToBooks() {
+    let allBooks = document.querySelectorAll('.bookCard');
+    allBooks.forEach((item) => {
+        let checkBox = document.createElement('input');
+        checkBox.setAttribute("type", "checkbox");
+        item.appendChild(checkBox);
+    });
+
+}*/
+
+function showBookInfo (thisBook) {
+    let infoDiv = document.createElement('div');
+
+    console.log(myBooks[thisBook.getAttribute("id")]);
+}
+
+function readStatus(bookCard) {
+    let currentId = bookCard.getAttribute("id");
+    let currDivChildren = document.getElementById(`${currentId}`).children;
+    let currReadBtn = currDivChildren.item(1);
+    currReadBtn.classList.toggle('read-unread');
+    console.log(bookCard);
+}
+
+function refreshID () {
+    let allBooks = document.querySelectorAll('.bookCard');
+    let i = 0;
+    allBooks.forEach((item) => {
+        item.setAttribute('id', i);
+        i++;
+});
+}
 
 function showForm() {
     form.classList.toggle('visible');
@@ -35,15 +134,35 @@ function addBook() {
     let title = document.getElementById('title').value;
     let author = document.getElementById('author').value;
     let year = document.getElementById('year').value;
-    let readStatus = document.getElementById('readStatus').value;
+    let readStatus = document.getElementById('readStatus').checked;
     let pages = document.getElementById('pages').value;
+
+    
+    if (title === "" || author === "" ||
+    year === "" || isNaN(year)) {
+        return 1;
+    }
 
     return(new Book(title, author, year, readStatus, pages));
 }
 
 newButton.addEventListener('click', showForm);
-addBookBtn.addEventListener('click', function () {
-    addBookToLibrary(addBook());
+
+//Click off form to hide it again
+/*document.addEventListener('click', function exitFormByClick(event) {;
+    console.log(event.target);
+    if (bod.contains(event.target)) {
+      //showForm();
+    }
+  });*/
+  
+
+/*delButton.addEventListener('click', addSelectorsToBooks);*/
+addBookBtn.addEventListener('click', function e() {
+    let x = addBookToLibrary(addBook());
+    if (x === 1) {
+        e();
+    }
     showForm();
 });
 
